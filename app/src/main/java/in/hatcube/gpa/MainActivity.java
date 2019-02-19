63c9c57e570a40222958b789dbf68ff44477eb74
akshay-zestl <akshay@zestl.com>
2019-02-19 18:11:10 +0530

    - Commit source code

---
+++ app/src/main/java/in/hatcube/gpa/MainActivity.java
@@ -0,0 +1,37 @@
+package in.hatcube.gpa;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button signInButton, signUpButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        signInButton = findViewById(R.id.signin);
+        signUpButton = findViewById(R.id.signup);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LogupActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
