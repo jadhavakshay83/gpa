d9df4cf0cd3217d9415d4c0c4d4fb1d83f9d2350
akshay-zestl <akshay@zestl.com>
2019-02-25 19:24:41 +0530

    - New images for cuid click scheme added

--- app/src/main/java/in/hatcube/gpa/ClickimgActivity.java
+++ app/src/main/java/in/hatcube/gpa/ClickimgActivity.java
@@ -1,13 +1,122 @@
 package in.hatcube.gpa;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+import java.util.Random;
 
 public class ClickimgActivity extends AppCompatActivity {
 
+    RelativeLayout interactiveImg;
+    LinearLayout clickableArea;
+    int firstImg = 0,secondImg = 0,thirdImg = 0,firstImgClick,secondImgClick,thirdImgClick;
+    boolean clickOne, clickTwo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clickimg);
+
+        //Set any random image as background
+        interactiveImg = findViewById(R.id.interactive_img);
+        firstImg = getRandomNumber(10,0, 0);
+        interactiveImg.setBackground(GetImage(getApplicationContext(),"ccs_"+firstImg));
+
+        //Set any random area as clickable
+        clickableArea = findViewById(R.id.clickable_area);
+        firstImgClick = getRandomNumber(4, 0, 0);
+        setClickableArea(firstImgClick);
+
+        clickableArea.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(clickOne && clickTwo) {
+//                    AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
+//                    builder.setTitle("Success!");
+//                    builder.setMessage("Sign Up Successful, Please login to continue");
+//                    builder.setPositiveButton("Login", new DialogInterface.OnClickListener() {
+//                        @Override
+//                        public void onClick(DialogInterface dialog, int which) {
+                    Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                    startActivity(intent);
+//                        }
+//                    });
+//                    AlertDialog dialog = builder.create();
+//                    dialog.show();
+                }
+
+                if(secondImg == 0) {
+                    secondImg = getRandomNumber(10,firstImg,0);
+                    interactiveImg.setBackground(GetImage(getApplicationContext(),"ccs_"+secondImg));
+
+                    secondImgClick = getRandomNumber(4, firstImgClick, 0);
+                    setClickableArea(secondImgClick);
+                    clickOne = true;
+                } else if(thirdImg == 0) {
+                    thirdImg = getRandomNumber(10, firstImg, secondImg);
+                    interactiveImg.setBackground(GetImage(getApplicationContext(),"ccs_"+thirdImg));
+
+                    thirdImgClick = getRandomNumber(4, firstImgClick, secondImgClick);
+                    setClickableArea(thirdImgClick);
+                    clickTwo = true;
+                }
+            }
+        });
     }
+
+    public static int getRandomNumber(int bound,int except, int except1) {
+        Random rand = new Random();
+        // Obtain a number between [0 - 9].
+        int number = rand.nextInt(bound);
+        // Add 1 to the result to get a number from the required range
+        // (i.e., [1 - 10]).
+        number += 1;
+        if(number == except || number == except1){
+            getRandomNumber(bound,except,except1);
+        }
+        return number;
+    }
+
+    public static Drawable GetImage(Context c, String ImageName) {
+        return c.getResources().getDrawable(c.getResources().getIdentifier(ImageName, "drawable", c.getPackageName()));
+    }
+
+    public void setClickableArea(int position) {
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(500,700);
+        switch (position) {
+            case 1:
+                params.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+                params.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+                break;
+            case 2:
+                params.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+                params.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+                clickableArea.setLayoutParams(params);
+                break;
+            case 3:
+                params.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+                params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+                clickableArea.setLayoutParams(params);
+                break;
+            case 4:
+                params.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+                params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+                clickableArea.setLayoutParams(params);
+                break;
+            default:
+                break;
+        }
+        clickableArea.setLayoutParams(params);
+    }
+
 }
